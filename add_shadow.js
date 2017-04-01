b8cd91541f6fa5df25361330c1fd1330ef4c0872
orangain <orangain@gmail.com>
2017-04-01 22:56:43 +0900

    Add script to add shadow

---
+++ add_shadow.js
@@ -0,0 +1,30 @@
+const svgpath = require('svgpath');
+
+const lineReader = require('readline').createInterface({
+  input: require('fs').createReadStream('/dev/stdin')
+});
+
+const dx = 5;
+const dy = 5;
+
+lineReader.on('line', function (line) {
+  if (line.indexOf('<svg') >= 0) {
+    line = line.replace(/width="([^"]*)"/, (match, w) => {
+      return `width="${(w - 0) + dx * 2}"`;
+    });
+    line = line.replace(/height="([^"]*)"/, (match, h) => {
+      return `height="${(h - 0) + dy * 2}"`;
+    });
+    line = line + `<filter id="drop-shadow">
+    <feGaussianBlur in="SourceAlpha" result="blur" stdDeviation="2" />
+    <feOffset result="offsetBlur" dx="2" dy="2" />
+    <feBlend in="SourceGraphic" in2="offsetBlur" mode="normal" />
+</filter>`;
+    line = line + `\n<g transform="translate(${dx} ${dy})">`;
+  } else if (line.indexOf('fill="#FEEC99"') >= 0) {
+    line = line.replace(/\/>/, ' filter="url(#drop-shadow)" />');
+  } else if (line.indexOf('</svg>') >= 0) {
+    line = '</g>\n' + line;
+  }
+  console.log(line);
+});
