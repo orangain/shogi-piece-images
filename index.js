70293122654559c91f346b4b057f6745ccf21ba4
orangain <orangain@gmail.com>
2017-04-02 09:58:50 +0900

    Export function to get SVG's data uri

---
+++ index.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const fs = require('fs');
+
+const DataURI = require('datauri');
+
+const IMAGES_WITH_SHADOW = '6_shadow';
+
+exports.getImagesSync = function () {
+  const images = {};
+  const dir = path.resolve(__dirname, IMAGES_WITH_SHADOW);
+  listSVGFilesSync(dir).forEach(filepath => {
+    const id = path.basename(filepath, path.extname(filepath));
+    images[id] = DataURI.sync(filepath);
+  });
+
+  return images;
+}
+
+function listSVGFilesSync(directory) {
+  return fs.readdirSync(directory)
+    .filter(name => name.endsWith('.svg'))
+    .map(name => path.resolve(directory, name));
+}
